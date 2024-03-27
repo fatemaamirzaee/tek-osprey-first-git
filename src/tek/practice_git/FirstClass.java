940ce17617e982e06153a0f12a931f26b9dba025
fatema mirzaee <fatema.na@tekschool.us>
2024-03-27 12:06:25 -0500

    second changes

--- src/tek/practice_git/FirstClass.java
+++ src/tek/practice_git/FirstClass.java
@@ -3,5 +3,8 @@ package tek.practice_git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git commit");
+        String firstText = "first commit was successfully done!";
+        String secondText = "Thank you git";
+        System.out.println(firstText + '\n' + secondText);
     }
 }
